61ad485b9465c1ecb2aadb284d0cdf0d494d91f8
Kinder Preet Singh <kp.cappy.kp@gmail.com>
2017-10-01 23:33:54 +0530

    building child themes from scratch using underscores lynda course

---
+++ Building theme from scratch.bat
@@ -0,0 +1,8 @@
+:: Go Into the gulp-dev folder to launch the gulp command
+start "" "C:\xampp\xampp-control.exe"
+pause
+cd /d C:\xampp\htdocs\wordpress\wp-content\themes\gulp-dev
+start "" "%PROGRAMFILES%\Git\bin\sh.exe" --login -i -c "gulp"
+start "" "C:\Program Files\Microsoft VS Code\Code.exe"
+start "" "https://www.lynda.com/WordPress-tutorials/Create-responsive-typography/491704/582723-4.html?autoplay=false"
+pause
